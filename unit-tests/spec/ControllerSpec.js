fd5d07d89a2ca348d8975b5edeff3a51d5332aaf
willm <william.munn@7digital.com>
2011-09-14 16:32:34 +0100

    wrote first test woo yay!

---
+++ unit-tests/spec/ControllerSpec.js
@@ -0,0 +1,27 @@
+﻿/*global describe: false, toString: false, beforeEach: false, it: false, HATEOAS_CONSOLE: false, expect: false */
+TICTACTOE = {};
+TICTACTOE.controller = function(){
+		return{getCurrentPlayer:function(){
+			return "0";
+		}}
+	};
+
+describe("Controller", function () {
+	"use strict";	
+	var controllerFactory = TICTACTOE.controller;
+	
+	describe("getCurrentPlayer", function () {
+	
+		
+		
+		it("should start with 0", function () {
+		
+			var controller = controllerFactory();
+			var player = controller.getCurrentPlayer();
+						
+			expect(player).toEqual("0");
+		});	
+		
+		
+	});
+});
