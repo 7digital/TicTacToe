19ad50c9e69fb7fcbb16deb76c722b02219c065a
willm <william.munn@7digital.com>
2011-09-21 15:55:55 +0100

    added Play method to controller

--- unit-tests/spec/ControllerSpec.js
+++ unit-tests/spec/ControllerSpec.js
@@ -1,26 +1,116 @@
 ﻿/*global describe: false, toString: false, beforeEach: false, it: false, HATEOAS_CONSOLE: false, expect: false */
 TICTACTOE = {};
 TICTACTOE.controller = function(){
-		return{getCurrentPlayer:function(){
-			return "0";
-		}}
+		return{
+			currentPlayer : "0",
+			
+			board : [[],[],[]],
+			
+			getCurrentPlayer : function() {
+					return this.currentPlayer;
+				},
+							
+			play : function(x, y) {
+				if (x > 2 || x < 0) {
+					throw {name: "Invalid square"};
+				}
+				if (y > 2 || y < 0) {
+					throw{name: "Invalid square"};
+				}
+				
+				if (this.board[x][y]) {
+					throw{name: "Square already played"};
+				}
+
+				this.board[x][y] = 1;
+				
+				this.currentPlayer = this.currentPlayer === "0" ? "X" : "0";
+				return "Player " + this.currentPlayer + "'s turn";
+			}
+		}
 	};
 
 describe("Controller", function () {
 	"use strict";	
-	var controllerFactory = TICTACTOE.controller;
-	
-	describe("getCurrentPlayer", function () {
-	
+	var controller;
+	beforeEach(function(){
+		controller = TICTACTOE.controller();
+	});
+
+	describe("play", function () {
+
+		it("should not throw an exception when you play a valid square", function() {
+			controller.play(1, 1);		
+		});
 		
+		it("should return 'Player X's turn' if it's player X's turn next", function() {
+			var result = controller.play(1, 1);	
+			expect(result).toEqual("Player X's turn");
+		});
+		
+		it("should return 'Player 0's turn' if it's player 0's turn next", function() {
+			var result = controller.play(1, 1);	
+			result = controller.play(1, 2);	
+			expect(result).toEqual("Player 0's turn");
+		});
+		
+		describe("fowl", function () {
+		
+			it("should throw an exception when the square is to the right of the board", function() {
+				expect(function() {
+					controller.play(4,1);
+				}).toThrow({name: "Invalid square"});
+			});
+			
+			it("should throw an exception when the square is to the left of the board", function() {
+				expect(function() {
+					controller.play(-1,1);
+				}).toThrow({name: "Invalid square"});
+			});
+			
+			it("should throw an exception when the square is off the bottom of the board", function() {
+				expect(function() {
+					controller.play(1,4);
+				}).toThrow({name: "Invalid square"});
+			});
+			
+			it("should throw an exception when the square is off the top of the board", function() {
+				expect(function() {
+					controller.play(1,-1);
+				}).toThrow({name: "Invalid square"});
+			});
+			
+			it("should throw an exception when you play a square twice", function() {
+				expect(function() {
+					controller.play(1, 1);
+					controller.play(1, 1);
+				}).toThrow({name: "Square already played"});
+			});
+		});			
+	});
+	
+	describe("getCurrentPlayer", function () {		
 		
 		it("should start with 0", function () {
+			var player = controller.getCurrentPlayer();
+
+			expect(player).toEqual("0");
+		});
 		
-			var controller = controllerFactory();
+		it("should return X after the first move", function () {
+			controller.play(0, 0);
+			var player = controller.getCurrentPlayer();
+						
+			expect(player).toEqual("X");
+		});
+		
+		it("should return 0 after the second move", function () {
+			controller.play(0, 1);
+			controller.play(1, 1);
 			var player = controller.getCurrentPlayer();
 						
 			expect(player).toEqual("0");
-		});	
+		});
 		
 		
 	});
