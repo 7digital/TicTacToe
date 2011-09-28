cffa5e3ffe55d3881a62c61073493cc45d66a6e6
ghay <graham.hay@7digital.com>
2011-09-28 16:30:00 +0100

    haha

--- unit-tests/spec/UmpireSpec.js
+++ unit-tests/spec/UmpireSpec.js
@@ -1,9 +1,15 @@
 var TICTACTOE = TICTACTOE || {};
 TICTACTOE.umpire = function(){
+        var checkRows = function(board){return false;}
+        var checkColumns = function(board){return false;}
+        var checkDiagonals = function(board){ 
+            return (board[0][0]===board[1][1])&&(board[1][1]===board[2][2])&&(board[0][0]!==undefined);            
+        };
 		return{
 			checkForWinner : function(board) {
-				return true;
-			}
+				var winner = checkRows(board) || checkColumns(board) || checkDiagonals(board);
+                return winner;
+			}            
 		}
 	};
 
@@ -19,6 +25,11 @@ describe("Umpire", function () {
             var board = [['X',,],[,'X',],[,,'X']];
             var result = umpire.checkForWinner(board);
             expect(result).toEqual(true);
+        });        
+        it("has not a winner if board has not 3 in a row", function(){      
+            var board = [['X',,],[,,,],['X',,'X']];
+            var result = umpire.checkForWinner(board);
+            expect(result).toEqual(false);
         });        
 	});
 	
