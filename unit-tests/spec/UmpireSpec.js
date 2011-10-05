e9f1ab91d43acea1de953608f01e874894955bfb
willm <william.munn@7digital.com>
2011-10-05 16:01:42 +0100

    Test for X winning _and_ 0 winning

--- unit-tests/spec/UmpireSpec.js
+++ unit-tests/spec/UmpireSpec.js
@@ -36,53 +36,55 @@ describe("Umpire", function () {
 		umpire = TICTACTOE.umpire();
 	});
 
-	describe("checkForWinner", function () {        
-        it("has a winner if board has 3 in a row diagonally tlbr", function(){      
-            var board = [['X',,],[,'X',],[,,'X']];
-            var result = umpire.checkForWinner(board);
-            expect(result).toEqual(true);
-        });
-		it("has a winner if board has 3 in a row diagonally trbl", function(){      
-            var board = [[,,'X'],[,'X',],['X',,]];
-            var result = umpire.checkForWinner(board);
-            expect(result).toEqual(true);
-        });
-		it("has a winner if board has 3 in a row horizontal top", function(){      
-            var board = [['X',,],['X',,],['X',,]];
-            var result = umpire.checkForWinner(board);
-            expect(result).toEqual(true);
-        }); 
-		it("has a winner if board has 3 in a row horizontal middle", function(){      
-            var board = [[,'X',],[,'X',],[,'X',]];
-            var result = umpire.checkForWinner(board);
-            expect(result).toEqual(true);
-        });
-		it("has a winner if board has 3 in a row horizontal bottom", function(){      
-            var board = [[,,'X'],[,,'X'],[,,'X']];
-            var result = umpire.checkForWinner(board);
-            expect(result).toEqual(true);
-        }); 
-		it("has a winner if board has 3 in a row vertical left", function(){      
-            var board = [['X','X','X'],[,,],[,,]];
-            var result = umpire.checkForWinner(board);
-            expect(result).toEqual(true);
-        }); 
-		it("has a winner if board has 3 in a row vertical middle", function(){      
-            var board = [[,,],['X','X','X'],[,,]];
-            var result = umpire.checkForWinner(board);
-            expect(result).toEqual(true);
-        });
-		it("has a winner if board has 3 in a row vertical right", function(){      
-            var board = [[,,],[,,],['X','X','X']];
-            var result = umpire.checkForWinner(board);
-            expect(result).toEqual(true);
-        });
+	describe("checkForWinner", function () {
+	
+		['X','0'].forEach(function(player){
+		
+			describe(player + " winning", function() {
+				it("has a winner if board has 3 in a row diagonally tlbr", function(){      
+					var board = [[player,,],[,player,],[,,player]];
+					var result = umpire.checkForWinner(board);
+					expect(result).toEqual(true);
+				});
+				it("has a winner if board has 3 in a row diagonally trbl", function(){      
+					var board = [[,,player],[,player,],[player,,]];
+					var result = umpire.checkForWinner(board);
+					expect(result).toEqual(true);
+				});
+				it("has a winner if board has 3 in a row horizontal top", function(){      
+					var board = [[player,,],[player,,],[player,,]];
+					var result = umpire.checkForWinner(board);
+					expect(result).toEqual(true);
+				}); 
+				it("has a winner if board has 3 in a row horizontal middle", function(){      
+					var board = [[,player,],[,player,],[,player,]];
+					var result = umpire.checkForWinner(board);
+					expect(result).toEqual(true);
+				});
+				it("has a winner if board has 3 in a row horizontal bottom", function(){      
+					var board = [[,,player],[,,player],[,,player]];
+					var result = umpire.checkForWinner(board);
+					expect(result).toEqual(true);
+				}); 
+				it("has a winner if board has 3 in a row vertical left", function(){      
+					var board = [[player,player,player],[,,],[,,]];
+					var result = umpire.checkForWinner(board);
+					expect(result).toEqual(true);
+				}); 
+				it("has a winner if board has 3 in a row vertical middle", function(){      
+					var board = [[,,],[player,player,player],[,,]];
+					var result = umpire.checkForWinner(board);
+					expect(result).toEqual(true);
+				});
+				it("has a winner if board has 3 in a row vertical right", function(){      
+					var board = [[,,],[,,],[player,player,player]];
+					var result = umpire.checkForWinner(board);
+					expect(result).toEqual(true);
+				});
+			});
+		});
+        
         it("has not a winner if board has not 3 in a row", function(){      
-            var board = [['X',,],[,,],['X',,'X']];
-            var result = umpire.checkForWinner(board);
-            expect(result).toEqual(false);
-        });
-		it("has a winner if board has 3 in a row", function(){      
             var board = [['X',,],[,,],['X',,'X']];
             var result = umpire.checkForWinner(board);
             expect(result).toEqual(false);
