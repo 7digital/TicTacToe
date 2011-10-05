d05fc2708ad6665639ebbf8c2f1d3ea287eb2cfa
willm <william.munn@7digital.com>
2011-10-05 16:11:30 +0100

    Refactored in some privacy

--- unit-tests/spec/ControllerSpec.js
+++ unit-tests/spec/ControllerSpec.js
@@ -1,38 +1,33 @@
 ﻿var TICTACTOE = TICTACTOE || {};
 TICTACTOE.controller = function(umpire){
-		return{
-			currentPlayer : "0",
-			
-			board : [[],[],[]],
-			
-			getCurrentPlayer : function() {
-					return this.currentPlayer;
-				},
-							
-			play : function(x, y) {
-				if (x > 2 || x < 0) {
-					throw {name: "Invalid square"};
-				}
-				if (y > 2 || y < 0) {
-					throw{name: "Invalid square"};
-				}
-				
-				if (this.board[x][y]) {
-					throw{name: "Square already played"};
-				}
+	var currentPlayer = "0",
+		board = [[],[],[]];
 
-				this.board[x][y] = this.currentPlayer;
-                
-                var hasAWinner = umpire.checkForWinner(this.board); 
-                if(hasAWinner) {
-                    return "Player " + this.currentPlayer + " rules!";
-                }
-				
-				this.currentPlayer = this.currentPlayer === "0" ? "X" : "0";
-				return "Player " + this.currentPlayer + "'s turn";
+	return {
+		play : function(x, y) {
+			if (x > 2 || x < 0) {
+				throw {name: "Invalid square"};
 			}
+			if (y > 2 || y < 0) {
+				throw{name: "Invalid square"};
+			}
+			
+			if (board[x][y]) {
+				throw{name: "Square already played"};
+			}
+
+			board[x][y] = currentPlayer;
+			
+			var hasAWinner = umpire.checkForWinner(board); 
+			if(hasAWinner) {
+				return "Player " + currentPlayer + " rules!";
+			}
+			
+			currentPlayer = currentPlayer === "0" ? "X" : "0";
+			return "Player " + currentPlayer + "'s turn";
 		}
 	};
+};
 
 describe("Controller", function () {
 	"use strict";	
@@ -53,17 +48,21 @@ describe("Controller", function () {
 			controller.play(1, 1);		
 		});
 		
-		it("should return 'Player X's turn' if it's player X's turn next", function() {
-			var result = controller.play(1, 1);	
-			expect(result).toEqual("Player X's turn");
-		});
+		describe("returns next player", function () {
 		
-		it("should return 'Player 0's turn' if it's player 0's turn next", function() {
-			var result = controller.play(1, 1);	
-			result = controller.play(1, 2);	
-			expect(result).toEqual("Player 0's turn");
+			it("should return 'Player X's turn' if it's player X's turn next", function() {
+				var result = controller.play(1, 1);	
+				expect(result).toEqual("Player X's turn");
+			});
+			
+			it("should return 'Player 0's turn' if it's player 0's turn next", function() {
+				var result = controller.play(1, 1);	
+				result = controller.play(1, 2);	
+				expect(result).toEqual("Player 0's turn");
+			});
+
 		});
-		
+			
 		describe("fowl", function () {
 		
 			it("should throw an exception when the square is to the right of the board", function() {
@@ -118,29 +117,4 @@ describe("Controller", function () {
         });
 	});
 	
-	describe("getCurrentPlayer", function () {		
-		
-		it("should start with 0", function () {
-			var player = controller.getCurrentPlayer();
-
-			expect(player).toEqual("0");
-		});
-		
-		it("should return X after the first move", function () {
-			controller.play(0, 0);
-			var player = controller.getCurrentPlayer();
-						
-			expect(player).toEqual("X");
-		});
-		
-		it("should return 0 after the second move", function () {
-			controller.play(0, 1);
-			controller.play(1, 1);
-			var player = controller.getCurrentPlayer();
-						
-			expect(player).toEqual("0");
-		});
-		
-		
-	});
 });
